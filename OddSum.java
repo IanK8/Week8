26ac1f10abd6be0341784d5df9a9a350cf855bdd
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-03-27 14:41:45 -0400

    what the sigma

--- OddSum.java
+++ OddSum.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class Factorial {
+public class OddSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number: ");
         int number = scanner.nextInt();
-        int factorial = 1;
-        for (int i = 1; i <= number; i++) {
-            factorial += i;
+        int odd = 1;
+        for (int i = 1; i <= number; i+=2) {
+            odd += i;
         }
-        System.out.println("Factorial of " + number + " is: " + factorial);
+        System.out.println("Factorial with odds " + number + " is: " + odd);
         scanner.close();
     }
 }
